bffc7bc1bb428993fea73c297a4f201662f1bc84
Dharshini R <dharshiniramesh2612@gmail.com>
2026-07-09 19:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (62.00%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        richest = 0
+
+        for customer in accounts:
+            wealth = sum(customer)
+            richest = max(richest, wealth)
+
+        return richest
+        
